15a142f49059a2815cf96565b64e8d2f3e1bbe89
egecan <118941751+egecansen@users.noreply.github.com>
2025-06-16 21:05:48 +0300

    Initial commit

---
+++ src/main/java/egecan/sahibinden_case_study/SahibindenCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package egecan.sahibinden_case_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SahibindenCaseStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SahibindenCaseStudyApplication.class, args);
+	}
+
+}
